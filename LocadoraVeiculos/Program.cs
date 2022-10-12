d0baffe4a420de690a87e35e24e412cf9545c33b
ThiagoAquilaDevweb <thiagotmbrasil@gmail.com>
2022-10-12 10:52:44 -0300

    Solução sem interfaces

---
+++ LocadoraVeiculos/Program.cs
@@ -0,0 +1,33 @@
+﻿using LocadoraVeiculos.Entities;
+using LocadoraVeiculos.Services;
+using LocadoraVeiculos.Services;
+using System.Globalization;
+
+namespace LocadoraVeiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            RentalServices rentalServices = new RentalServices(hour, day);
+
+            rentalServices.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
