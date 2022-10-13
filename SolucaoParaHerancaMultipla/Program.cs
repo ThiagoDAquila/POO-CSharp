b4ea747ba34743b03ede31c5356581a10b1df8b4
ThiagoAquilaDevweb <thiagotmbrasil@gmail.com>
2022-10-13 16:48:05 -0300

    Program.Cs atualizado

--- SolucaoParaHerancaMultipla/Program.cs
+++ SolucaoParaHerancaMultipla/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SolucaoParaHerancaMultipla.Devices;
+
+namespace SolucaoParaHerancaMultipla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer() { SerialNumber = 2801 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+
+            Scanner s = new Scanner() { SerialNumber = 3003 };
+            s.ProcessDoc("My email");
+            Console.WriteLine(s.Scan());
+
+            ComboDevice c = new ComboDevice() { SerialNumber = 1001 };
+            c.ProcessDoc("My dissertation");
+            c.Print("My dissertation");
+            Console.WriteLine(c.Scan());
+        }
+    }
+}
