df74c34305b3c51b68c457964c94e0e24bb8280d
ThiagoAquilaDevweb <thiagotmbrasil@gmail.com>
2022-10-21 17:05:31 -0300

    Solução com Dictionary

---
+++ QuantityVotes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace QuantityVotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidate = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+                }
+
+                
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
