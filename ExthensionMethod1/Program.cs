e4e62d7fbb68592738f0ad1317ff48b945cefe30
ThiagoAquilaDevweb <thiagotmbrasil@gmail.com>
2022-10-24 18:15:31 -0300

    Solução com Exthension Methods

---
+++ ExthensionMethod1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExthensionMethod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 10, 24, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
